47942c7fe731e5e0a07adf46eabd080e91e61689
masudrana2803 <masudrana2803@gmail.com>
2025-08-11 14:00:14 +0600

    function_toDo

--- Function_explained/function.js
+++ Function_explained/function.js
@@ -1,3 +1,5 @@
+let container1 = document.getElementById('container1');
+// let appear=document.getElementById('appear')
 function appearThem() {
     // Create a new div element
     const appear = document.createElement('div');
@@ -18,6 +20,37 @@ function appearThem() {
             <p>I am menu4</p>
         </div>
     `;
-    // Append the div to the body (or another container)
-    document.body.appendChild(appear);
+        container1.appendChild(appear);
+
+}
+
+function addItems (){
+    const todo=document.getElementById('todo')
+    const inputType=document.getElementById('inputType')
+
+    const addbtn = document.createElement('div')
+    addbtn.classList.add('addbtn')
+    addbtn.innerHTML=`
+    <div class="totalToDo">
+    <input type="text" class="singleAdd">
+    <button id="editBtn" onclick="editThis()">Edit</button>
+    <button id="updateBtn" onclick="updateThis()">Update</button>
+    <button id="deleteBtn" onclick="deleteThis()">Delete</button>
+    </div>
+    `
+    inputType.value=''
+
+    addbtn.querySelector('.singleAdd').value = inputType.value
+    todo.appendChild(addbtn)
+
+
+    let editBtn = document.getElementById('editBtn')
+    let updateBtn = document.getElementById('updateBtn')
+    let deleteBtn = document.getElementById('deleteBtn')
+
+    editBtn.addEventListener('click',()=>{
+        editBtn.style='background:red'
+    })
+
+
 }
